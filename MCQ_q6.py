7cec73ecd8a3155be56d5118ef8a294d786bb1fe
istone1000 <116089208+istone1000@users.noreply.github.com>
2023-01-05 18:09:10 +0000

    Update MCQ_q6.py

--- MCQ_q6.py
+++ MCQ_q6.py
@@ -1,10 +1,11 @@
 teachers = { }
 
-#answers
+#solution
 
-teachers = { "Maths":"Irene",
-          "Computer Science": "Sinéad",
-          "Science":"Neil"}
+#this creates a new dictionary, overwriting the existing teachers dictionary instead of adding new key-value pairs to it.
+#teachers = { "Maths":"Irene",
+#          "Computer Science": "Sinéad",
+#          "Science":"Neil"}
 
 # teachers["Maths"]:"Irene"
 # teachers["Computer Science"]:"Sinéad"
@@ -14,10 +15,9 @@ teachers = { "Maths":"Irene",
 # teachers{"Computer Science"="Sinéad"}
 # teachers{"Science"="Neil"}
 
+#correct solution
 teachers["Maths"]="Irene"
 teachers["Computer Science"]="Sinéad"
 teachers["Science"]="Neil"
 
-print(teachers)
-del teachers["Maths"]
 print(teachers)
